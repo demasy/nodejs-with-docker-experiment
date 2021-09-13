a1a6b2d9138d75cf9b913caa326a67d5f70f3966
Ahmed El-Demasy <domssa@live.com>
2021-09-13 15:43:52 +0300

    update SQL

--- src/apis/test.js
+++ src/apis/test.js
@@ -2,13 +2,11 @@ const oracledb = require('oracledb');
 const database = require('../services/database.js');
 
 const baseQuery =
-    `SELECT TO_CHAR(CURRENT_DATE, 'DD-Mon-YYYY HH24:MI') AS CD FROM DUAL`;
+    `SELECT TO_CHAR(CURRENT_DATE, 'DD-Mon-YYYY HH24:MI') AS today FROM DUAL`;
 
-async function find(context) {
+module.exports.find = async (context) => {
     let query = baseQuery;
     const binds = {};
     const result = await database.execute(query, binds);
     return result.rows;
 }
-
-module.exports.find = find;
