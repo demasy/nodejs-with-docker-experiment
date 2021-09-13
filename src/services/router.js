9f2e43f0d518361c0a6ec96fe58feb3ad381449a
Ahmed El-Demasy <domssa@live.com>
2021-09-13 15:31:58 +0300

    create router service

---
+++ src/services/router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = new express.Router();
+const test = require('../controllers/test.js');
+
+router.route('/test/:id?')
+  .get(test.get);
+
+module.exports = router;
--- src/services/webserver.js
+++ src/services/webserver.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const webServerConfig = require('../config/webserver.js');
+const router = require('./router.js');
 
 let httpServer;
 
@@ -9,9 +10,11 @@ function initialize() {
         const app = express();
         httpServer = http.createServer(app);
 
-        app.get('/', (req, res) => {
-            res.end('Hello World!');
-        });
+        app.use(express.json({
+            reviver: reviveJson
+        }));
+
+        app.use('/demasy/api', router);
 
         httpServer.listen(webServerConfig.port, err => {
             if (err) {
@@ -27,14 +30,25 @@ function initialize() {
 module.exports.initialize = initialize;
 
 
-module.exports.close = ()=> {
+module.exports.close = () => {
     return new Promise((resolve, reject) => {
-      httpServer.close((err) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve();
-      });
+        httpServer.close((err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
     });
+}
+
+const iso8601RegExp = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/;
+
+function reviveJson(key, value) {
+  // revive ISO 8601 date strings to instances of Date
+  if (typeof value === 'string' && iso8601RegExp.test(value)) {
+    return new Date(value);
+  } else {
+    return value;
   }
+}
