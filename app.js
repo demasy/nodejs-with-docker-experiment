7afb9687535e1cfbe548d8d9986258bb2e752bd0
Ahmed El-Demasy <domssa@live.com>
2021-09-13 12:42:57 +0300

    create http server

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+const webServer = require('./src/services/webserver.js');
+
+// const webServerConfig = require('../config/web-server.js');
+
+async function startup() {
+  console.log('Starting application');
+
+  try {
+    console.log('Initializing web server module');
+
+    await webServer.initialize();
+  } catch (err) {
+    console.error(err);
+
+    process.exit(1); // Non-zero failure code
+  }
+}
+
+
+startup();
---
+++ src/services/webserver.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const express = require('express');
+const webServerConfig = require('../config/webserver.js');
+
+let httpServer;
+
+function initialize() {
+    return new Promise((resolve, reject) => {
+        const app = express();
+        httpServer = http.createServer(app);
+
+        app.get('/', (req, res) => {
+            res.end('Hello World!');
+        });
+
+        httpServer.listen(webServerConfig.port, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            console.log('Web server listening on localhost:${webServerConfig.port}');
+            resolve();
+        });
+    });
+}
+
+module.exports.initialize = initialize;
